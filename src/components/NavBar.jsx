a6c64a5a746e49bdfbdf400c2c94e266bf0dbbc0
Keilani Uehara <keilani.uehara@gmail.com>
2026-04-27 16:18:46 -0700

    added in component file and navbar jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="nav">
+      <div className="logo">MySite</div>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Menu */}
+      <ul className={open ? "nav-links open" : "nav-links"}>
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
